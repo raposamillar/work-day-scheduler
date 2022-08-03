c626d385568277a57adc82f3f7e45cbadeab3d76
raposamillar <lisa.raposamillar@gmail.com>
2022-08-02 22:59:31 -0400

    add time blocks with standard business hours

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,26 @@
-const main = document.querySelector('.container');
-$('#currentDay').text(moment().format("lll"));
+const main = document.querySelector(".container");
+$("#currentDay").text(moment().format("lll"));
 
+const hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+// current military time:
+let H = moment().format("H");
+
+hours.forEach((hour,i) => {
+// The row hour:
+    let rH = i+9;
+    
+    main.innerHTML += `
+        <div class="row">
+            <div class="hour">
+                ${hour}
+            </div>
+            <textarea class="${rH<H ? 'past' : rH>H ? 'future' : 'present'}">
+            
+            </textarea>
+            <div class="saveBtn">
+            
+            </div>
+        </div>
+    `
+});
